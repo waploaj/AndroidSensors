71a20634128a093f2d268232f4cf9a829ad2b9e1
Abdallah Abdillah <waploaj@icloud.com>
2022-02-19 02:43:45 +0300

    update stepcounter sensor

---
+++ app/src/main/java/com/waploaj/proximitysensor/LightSensor.kt
@@ -0,0 +1,11 @@
+package com.waploaj.proximitysensor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LightSensor : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_light_sensor)
+    }
+}
