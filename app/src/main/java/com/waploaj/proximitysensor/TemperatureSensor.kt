b6baf78069a1724aac888f4b637fa43014ee74c7
Abdallah Abdillah <waploaj@icloud.com>
2022-02-21 00:53:20 +0300

    update gitignore

---
+++ app/src/main/java/com/waploaj/proximitysensor/TemperatureSensor.kt
@@ -0,0 +1,11 @@
+package com.waploaj.proximitysensor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TemperatureSensor : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_temperature_sensor)
+    }
+}
