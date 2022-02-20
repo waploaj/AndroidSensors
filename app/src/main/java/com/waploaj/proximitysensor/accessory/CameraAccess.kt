8995e9842799e057f1061570cb374480ce8069ee
Abdallah Abdillah <waploaj@icloud.com>
2022-02-20 17:10:40 +0300

    allow camera, microphone and storage permission

---
+++ app/src/main/java/com/waploaj/proximitysensor/accessory/CameraAccess.kt
@@ -0,0 +1,11 @@
+package com.waploaj.proximitysensor.accessory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CameraAccess : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_camera_access)
+    }
+}
---
+++ app/src/main/java/com/waploaj/proximitysensor/accessory/MicrophoneAccess.kt
@@ -0,0 +1,11 @@
+package com.waploaj.proximitysensor.accessory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MicrophoneAccess : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_microphone_access)
+    }
+}
