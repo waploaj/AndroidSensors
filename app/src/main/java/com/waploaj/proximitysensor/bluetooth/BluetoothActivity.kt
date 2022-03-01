c227dd5a1c8532bbea471b8227c14cc8c37d5624
Abdallah Abdillah <waploaj@icloud.com>
2022-03-02 01:12:02 +0300

    update gps precise location

---
+++ app/src/main/java/com/waploaj/proximitysensor/bluetooth/BluetoothActivity.kt
@@ -0,0 +1,11 @@
+package com.waploaj.proximitysensor.bluetooth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BluetoothActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bluetooth)
+    }
+}
